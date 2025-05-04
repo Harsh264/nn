362579b2e945bf905d84219acd1235eded479d55
Harsh264 <hv26418@gmail.com>
2025-05-04 21:46:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-green-100 to-purple-100">
+      <Nav />
+      <Hero />
+    </div>
+  )
+}
